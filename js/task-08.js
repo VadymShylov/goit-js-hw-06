20ee83b78a59e2b1c6e881cef5752897c94684a1
VadymShylov <shylovvadym@gmail.com>
2022-07-24 12:59:19 +0100

    added task

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,34 @@
+// Задание 8
+// Напиши скрипт управления формой логина.
+// Обработка отправки формы form.login-form должна быть по событию submit.
+// При отправке формы страница не должна перезагружаться.
+// Если в форме есть незаполненные поля, выводи alert с предупреждением о том, что все поля должны быть заполнены.
+// Если пользователь заполнил все поля и отправил форму, собери значения полей в обьект, где имя поля будет именем свойства, а значение поля - значением свойства. Для доступа к элементам формы используй свойство elements.
+// Выведи обьект с введенными данными в консоль и очисти значения полей формы методом reset.
 
+const formLogin = {
+  form: document.querySelector(".login-form"),
+};
+formLogin.form.addEventListener("submit", onFormSubmit);
+function formSubmit(events) {
+  events.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = events.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    alert("All fields must be filled!!!");
+  } else {
+    const formData = new FormData(events.currentTarget);
+    const loginFormData = {};
+
+    formData.forEach((value, name) => {
+      loginFormData[name] = value;
+    });
+
+    console.log(loginFormData);
+  }
+
+  events.currentTarget.reset();
+}
--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,15 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const elementOfBody = document.querySelector("body");
+const spanElement = document.querySelector(".color");
+const buttonElement = document.querySelector(".change-color");
+
+buttonElement.addEventListener("click", onClickChangeColor);
+
+function onClickChangeColor() {
+  elementOfBody.style.backgroundColor = getRandomHexColor();
+  spanElement.textContent = elementOfBody.style.backgroundColor;
+}
+// Задание 9
+// Напиши скрипт, который изменяет цвета фона элемента <body> через инлайн стиль при клике на button.change-color и выводит значение цвета в span.color.
